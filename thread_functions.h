2dbf8b8de56a2524c3b57da71f81706be7eed3aa
Dgabri <gabriele.deri@gmail.com>
2023-05-18 22:04:16 +0200

    implemented threads functions in a separate file

---
+++ thread_functions.h
@@ -0,0 +1,13 @@
+#include "include.h"
+
+// capo scrittore function
+void *capo_scrittore(void *arg);
+
+// scrittore function
+void *thread_scrittore(void *arg);
+
+// capo lettore function
+void *capo_lettore(void *arg);
+
+// lettore function
+void *thread_lettore(void *arg);
