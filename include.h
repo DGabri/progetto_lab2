20df50e7884f9ae90e382a27eb9b24fe6a92ab14
Dgabri <gabriele.deri@gmail.com>
2023-05-18 22:03:10 +0200

    cleaned all includes

--- include.h
+++ include.h
@@ -1,9 +1,17 @@
 #include "xerrori.h"
 #include <search.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <search.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define line __LINE__
 #define file __FILE__
 #define Max_sequence_length 2048
 
-#include "buffer.h"
 #include "hashtable.h"
+#include "buffer.h"
+
